bdf0158e6efe9f233ebcb1d75605f2674be5d0c4
ThgMelo <es.thg.melo@gmail.com>
2022-06-06 06:01:55 -0300

    Modais

--- assets/scripts/keyboard.js
+++ assets/scripts/keyboard.js
@@ -1,10 +1,26 @@
 document.addEventListener("keydown", function(event){
+    if(event.keyCode != 122){
+        event.preventDefault();
+    }
+    
     if(event.keyCode == 120){
         abrir_sai();
     }
 
     if(event.keyCode == 121){
         abrir_ala();
+    }
+
+    if(event.keyCode == 27){
+        fechaModalAberto()
+    }
+
+    if(event.keyCode == 112){
+        document.querySelector("#modal-apontamento1").classList.add("visible");
+    }
+
+    if(event.keyCode == 113){
+        document.querySelector("#modal-rmp").classList.add("visible");
     }
 })
 
--- assets/scripts/modais.js
+++ assets/scripts/modais.js
@@ -1,18 +1,27 @@
-const botaoEl = document.querySelector("button");
-botaoEl.addEventListener("click", abreModal);
+const botoes = document.querySelectorAll("[data-abre-modal]");
+for( let botaoEl of botoes){
+    botaoEl.addEventListener("click", abreModal);
+}
 
 function abreModal(e){
-    const modalEl = document.querySelector(".modal");
+    const botaoClicadoEl = e.currentTarget;
+    const seletorDoModal = botaoClicadoEl.dataset.abreModal;
+    const modalEl = document.querySelector(seletorDoModal);
     modalEl.classList.add("visible");
 }
 
 const botoesDeFechar = document.querySelectorAll(".fechar-modal");
 botoesDeFechar.forEach(fechaEl => fechaEl.addEventListener("click", fechaModal));
 
-const mascara = document.querySelector("#mascara");
-mascara.addEventListener("click", fechaModal);
-
 function fechaModal(e){
-    const modalEl = document.querySelector(".modal");
+    const fecharModalEl = e.currentTarget;
+    const modalEl = fecharModalEl.closest(".modal");
     modalEl.classList.remove("visible");
+}
+
+const mascara = document.querySelector("#mascara");
+mascara.addEventListener("click", fechaModalAberto);
+
+function fechaModalAberto(){
+    document.querySelector(".modal.visible").classList.remove("visible");
 }
